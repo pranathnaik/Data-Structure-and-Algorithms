6fa383ec41bbd3767d212c2a279c208178636d8f
pranathnaik <pranathnaik12@gmail.com>
2020-10-29 22:11:31 +0530

    ksmall

---
+++ array/ksmall.cpp
@@ -0,0 +1,79 @@
+/*Given an array arr[] and a number K where K is smaller than size of array, the task is to find the Kth smallest element in the given array. It is given that all array elements are distinct.
+
+Input:
+The first line of input contains an integer T, denoting the number of testcases.
+Then T test cases follow.
+Each test case consists of three lines.
+First line of each testcase contains an integer N denoting size of the array.
+Second line contains N space separated integer denoting elements of the array.
+Third line of the test case contains an integer K.
+
+Output:
+Corresponding to each test case, print the kth smallest element in a new line.
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+1 <= T <= 100
+1 <= N <= 105
+1 <= arr[i] <= 105
+1 <= K <= N
+
+Example:
+Input:
+2                               //T
+6                               //N
+7 10 4 3 20 15                  //3 4 7 10 15 20
+3                               //K
+5
+7 10 4 20 15                    //4 7 10 15 20
+4
+Output:
+7
+15
+
+Explanation:
+Testcase 1: 3rd smallest element in the given array is 7.
+Testcase 2: 4th smallest elemets in the given array is 15.*/
+
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    int T, N, K;
+    cout << "enter the number of test cases";
+    cin >> T;
+    int arrt[T];
+    int temp;
+    for (int i = 0; i < T; i++)
+    {
+        cin >> N;
+        int arr[N];
+        for (int i = 0; i < N; i++)
+        {
+            cin >> arr[i];
+        }
+        for (int p = 0; p < N; p++)
+        {
+            for (int j = p + 1; j < N; j++)
+            {
+                if (arr[j] < arr[p])
+                {
+                    temp = arr[p];
+                    arr[p] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        cin >> K;
+        arrt[i] = arr[K-1]; 
+        cout << endl;
+    }
+        for (int i = 0; i < T; i++)
+        {
+            cout << arrt[i] << endl;
+        }
+}
